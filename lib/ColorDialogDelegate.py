4f040d484e16f5dd69e40f82a6ec2fd9322b6f4c
flurl <it@postgarage.at>
2013-07-08 14:31:35 +0200

    delegate für farbauswahl in einem tableview

---
+++ lib/ColorDialogDelegate.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+from PyQt4 import QtCore, QtGui, QtSql
+
+class ColorDialogDelegate(QtSql.QSqlRelationalDelegate):
+	"""this class shows a push button to open a QColorDialog"""
+	
+	def __init__(self, *args, **kwargs):
+		super(ColorDialogDelegate, self).__init__(*args, **kwargs)
+		self.colorColumn = -1
+		self.prevData = None
+
+	def createEditor(self, parent, option, index):
+		if index.column() == self.colorColumn:
+				button = QtGui.QPushButton(parent)
+				self.connect(button, QtCore.SIGNAL('clicked()'), lambda i=index: self.openColorDialog(index))
+				return button
+		return QItemDelegate.createEditor(self, parent, option, index)
+	
+	
+	def paint(self, painter, option, index):
+		opt = QtGui.QStyleOptionViewItem(option)
+		if index.column() == self.colorColumn:
+			color = QtGui.QColor(index.data().toString())
+			painter.fillRect(option.rect, color)
+		return super(ColorDialogDelegate, self).paint(painter, opt, index)
+	
+	
+	def editorEvent (self, event, model, option, index):
+		if index.column() == self.colorColumn:
+			self.prevData = index.data()
+		return super(ColorDialogDelegate, self).editorEvent(event, model, option, index)
+	
+	
+	def setColorColumn(self, col):
+		self.colorColumn = col
+		
+		
+	def openColorDialog(self, idx):
+		color = QtGui.QColorDialog.getColor()
+		if color.isValid():
+			idx.model().setData(idx, color.name())
+		else:
+			color = QtGui.QColor(self.prevData)
+			if color.isValid():
+				idx.model().setData(idx, color.name())
+		
+
+
