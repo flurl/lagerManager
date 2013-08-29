6d6880cd78ba2faf7ba7e5713afec2b38b79b9b0
flurl <it@postgarage.at>
2013-08-29 15:35:13 +0200

    DN stunden report erweitert um nachtarbeitszuschlag, trinkgeldpauschale und summer der bezüge

--- reports/dienstnehmerStunden.py
+++ reports/dienstnehmerStunden.py
@@ -4,6 +4,8 @@ from PyQt4 import QtCore, QtGui, QtSql
 from ui.reports.dienstnehmerStundenReport_gui import Ui_DienstnehmerStunden
 from textReport import TextReport
 
+from CONSTANTS import *
+
 
 
 class DienstnehmerStundenReport(TextReport):
@@ -38,23 +40,49 @@ class DienstnehmerStundenReport(TextReport):
 		print pStart, pEnd
 		query = """
 				select {fields}
-				from dienstnehmer, dienste, veranstaltungen
+				from dienstnehmer, dienste, veranstaltungen, beschaeftigungsbereiche
 				where 1=1
 				and die_dinid = din_id
 				and die_verid = ver_id
+				and din_bebid = beb_id
 				and ver_datum between '{pStart}' and '{pEnd}'
 				group by {groupByFields}
 				"""
 				
+		nachtarbeitszuschlagQuery = """
+			sum(if (
+				if(die_beginn < concat(date(die_beginn), ' 06:00:00'), 
+					if (concat(date(die_beginn), ' 06:00:00') >= die_ende, 
+					time_to_sec(timediff(die_ende, die_beginn)),
+					time_to_sec(timediff(concat(date(die_beginn), ' 06:00:00'), die_beginn))), 
+					if(die_beginn >= concat(date(die_beginn), ' 22:00:00'),
+					if (adddate(concat(date(die_beginn), ' 06:00:00'), INTERVAL 1 DAY) > die_ende, 
+						time_to_sec(timediff(die_ende, die_beginn)), 
+						time_to_sec(timediff(adddate(concat(date(die_beginn), ' 06:00:00'), INTERVAL 1 DAY), die_beginn))
+					),
+					if (adddate(concat(date(die_beginn), ' 06:00:00'), INTERVAL 1 DAY) > die_ende, 
+						time_to_sec(timediff(die_ende, concat(date(die_beginn), ' 22:00:00'))), 
+						time_to_sec(timediff(adddate(concat(date(die_beginn), ' 06:00:00'), INTERVAL 1 DAY), concat(date(die_beginn), ' 22:00:00')))
+					)
+					)
+				) > time_to_sec(timediff(die_ende, die_beginn))/2,
+				1,
+				0
+				)*%s)
+		"""%(NACHTARBEITSZUSCHLAG, )
+			 
+				
 		if reportType == u'Jährlich':
-			fields = 'din_name, din_nummer, sum((unix_timestamp(die_ende)-unix_timestamp(die_beginn))/3600) - sum(die_pause), sum(die_pause), count(*), sum((unix_timestamp(die_ende)-unix_timestamp(die_beginn))/3600)*din_stundensatz'
+			fields = 'din_name, din_nummer, sum((unix_timestamp(die_ende)-unix_timestamp(die_beginn))/3600) - sum(die_pause), sum(die_pause), count(*), (sum((unix_timestamp(die_ende)-unix_timestamp(die_beginn))/3600) - sum(die_pause))*din_stundensatz, ' + nachtarbeitszuschlagQuery + ', {TRINKGELDPAUSCHALE}*(sum((unix_timestamp(die_ende)-unix_timestamp(die_beginn))/3600) - sum(die_pause))*beb_trinkgeldpauschale, ' + nachtarbeitszuschlagQuery + ' + (sum((unix_timestamp(die_ende)-unix_timestamp(die_beginn))/3600) - sum(die_pause))*din_stundensatz + {TRINKGELDPAUSCHALE}*(sum((unix_timestamp(die_ende)-unix_timestamp(die_beginn))/3600) - sum(die_pause))*beb_trinkgeldpauschale '
 			groupByFields = 'din_id, din_name, din_nummer'
 		elif reportType == u'Monatlich':
-			fields = 'monthname(ver_datum), din_name, din_nummer, sum((unix_timestamp(die_ende)-unix_timestamp(die_beginn))/3600) - sum(die_pause), sum(die_pause), count(*), sum((unix_timestamp(die_ende)-unix_timestamp(die_beginn))/3600)*din_stundensatz'
+			fields = 'monthname(ver_datum), din_name, din_nummer, sum((unix_timestamp(die_ende)-unix_timestamp(die_beginn))/3600) - sum(die_pause), sum(die_pause), count(*), (sum((unix_timestamp(die_ende)-unix_timestamp(die_beginn))/3600) - sum(die_pause))*din_stundensatz, ' + nachtarbeitszuschlagQuery + ', {TRINKGELDPAUSCHALE}*(sum((unix_timestamp(die_ende)-unix_timestamp(die_beginn))/3600) - sum(die_pause))*beb_trinkgeldpauschale, ' + nachtarbeitszuschlagQuery + ' + (sum((unix_timestamp(die_ende)-unix_timestamp(die_beginn))/3600) - sum(die_pause))*din_stundensatz + {TRINKGELDPAUSCHALE}*(sum((unix_timestamp(die_ende)-unix_timestamp(die_beginn))/3600) - sum(die_pause))*beb_trinkgeldpauschale '
 			groupByFields = 'monthname(ver_datum), din_id, din_name, din_nummer'
 		else:
-			fields = 'ver_bezeichnung, ver_datum, din_name, din_nummer, sum((unix_timestamp(die_ende)-unix_timestamp(die_beginn))/3600) - sum(die_pause), sum(die_pause), count(*), sum((unix_timestamp(die_ende)-unix_timestamp(die_beginn))/3600)*din_stundensatz'
+			fields = 'ver_bezeichnung, ver_datum, din_name, din_nummer, sum((unix_timestamp(die_ende)-unix_timestamp(die_beginn))/3600) - sum(die_pause), sum(die_pause), count(*), (sum((unix_timestamp(die_ende)-unix_timestamp(die_beginn))/3600) - sum(die_pause))*din_stundensatz, ' + nachtarbeitszuschlagQuery + ', {TRINKGELDPAUSCHALE}*(sum((unix_timestamp(die_ende)-unix_timestamp(die_beginn))/3600) - sum(die_pause))*beb_trinkgeldpauschale, ' + nachtarbeitszuschlagQuery + ' + (sum((unix_timestamp(die_ende)-unix_timestamp(die_beginn))/3600) - sum(die_pause))*din_stundensatz + {TRINKGELDPAUSCHALE}*(sum((unix_timestamp(die_ende)-unix_timestamp(die_beginn))/3600) - sum(die_pause))*beb_trinkgeldpauschale '
 			groupByFields = 'ver_id, ver_bezeichnung, ver_datum, din_id, din_name, din_nummer'
+			
+		fields = fields.format(TRINKGELDPAUSCHALE=TRINKGELDPAUSCHALE)
 			
 		query = query.format(
 							fields=fields,
