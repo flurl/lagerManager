bb8fd1ae6047b61288e0b0626ef4188af3158960
flurl <it@postgarage.at>
2015-02-24 13:11:58 +0100

    update skript korrigiert V#1108

--- sql/upd0007.sql
+++ sql/upd0007.sql
@@ -1,4 +1,3 @@
 #V1107
-#V1106
 
-insert into config (cfg_key, cfg_valueI) values ('trinkgeldpauschale', 0.2725);
+insert into config (cfg_key, cfg_valueF) values ('trinkgeldpauschale', 0.2725);
--- version.py
+++ version.py
@@ -1,4 +1,4 @@
-VERSION=1107
+VERSION=1108
 
 if __name__ == "__main__":
 	print VERSION
