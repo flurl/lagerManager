479707e53b2a7e2a872459f1d243f75c6c182722
flurl <it@postgarage.at>
2017-04-14 19:00:32 +0200

    add error handling in update script in case the ADD COLUMN statements fail because the column already exists V#1158

--- sql/upd0011.sql
+++ sql/upd0011.sql
@@ -1,27 +1,39 @@
 #V1156
+delimiter ;;
+DROP PROCEDURE IF EXISTS upgrade_db;;
+create procedure upgrade_db ()
+begin
+    declare continue handler for 1060 
+    begin 
+    SELECT 'column exists';
+    end;
+    
+    alter table tische_aktiv add column tisch_externer_beleg bit NOT NULL;
 
-alter table tische_aktiv add column tisch_externer_beleg bit NOT NULL;
+    alter table tische_bondetails add column tisch_bondetail_istExternerBeleg bit NOT NULL;
 
-alter table tische_bondetails add column tisch_bondetail_istExternerBeleg bit NOT NULL;
+    alter table tische_bereiche add column tischbereich_rksv bit NOT NULL;
 
-alter table tische_bereiche add column tischbereich_rksv bit NOT NULL;
+    alter table rechnungen_basis add column rechnung_kassenidentifikation varchar(2000) NULL;
+    alter table rechnungen_basis add column rechnung_barumsatz_nr int NULL;
+    alter table rechnungen_basis add column rechnung_gesamt_umsatz decimal(18; 3) NULL;
+    alter table rechnungen_basis add column rechnung_zertifikat_id varchar(2000) NULL;
+    alter table rechnungen_basis add column rechnung_referenz int NULL;
+    alter table rechnungen_basis add column rechnung_signatur varchar(2000) NULL;
+    alter table rechnungen_basis add column rechnung_druckpfad varchar(2000) NULL;
+    alter table rechnungen_basis add column rechnung_mwst_normal decimal(18; 3) NULL;
+    alter table rechnungen_basis add column rechnung_mwst_ermaessigt1 decimal(18; 3) NULL;
+    alter table rechnungen_basis add column rechnung_mwst_ermaessigt2 decimal(18; 3) NULL;
+    alter table rechnungen_basis add column rechnung_mwst_null decimal(18; 3) NULL;
+    alter table rechnungen_basis add column rechnung_mwst_besonders decimal(18; 3) NULL;
+    alter table rechnungen_basis add column rechnung_gesamt_umsatz_enc varchar(2000) NULL;
+    alter table rechnungen_basis add column rechnung_rka varchar(2000) NULL;
+    alter table rechnungen_basis add column rechnung_vorherige_signatur varchar(2000) NULL;
+    alter table rechnungen_basis add column rechnung_beleg_kennzeichen varchar(2000) NULL;
+    alter table rechnungen_basis add column rechnung_istTrainingsBeleg bit NOT NULL;
 
-alter table rechnungen_basis add column rechnung_kassenidentifikation varchar(2000) NULL;
-alter table rechnungen_basis add column rechnung_barumsatz_nr int NULL;
-alter table rechnungen_basis add column rechnung_gesamt_umsatz decimal(18; 3) NULL;
-alter table rechnungen_basis add column rechnung_zertifikat_id varchar(2000) NULL;
-alter table rechnungen_basis add column rechnung_referenz int NULL;
-alter table rechnungen_basis add column rechnung_signatur varchar(2000) NULL;
-alter table rechnungen_basis add column rechnung_druckpfad varchar(2000) NULL;
-alter table rechnungen_basis add column rechnung_mwst_normal decimal(18; 3) NULL;
-alter table rechnungen_basis add column rechnung_mwst_ermaessigt1 decimal(18; 3) NULL;
-alter table rechnungen_basis add column rechnung_mwst_ermaessigt2 decimal(18; 3) NULL;
-alter table rechnungen_basis add column rechnung_mwst_null decimal(18; 3) NULL;
-alter table rechnungen_basis add column rechnung_mwst_besonders decimal(18; 3) NULL;
-alter table rechnungen_basis add column rechnung_gesamt_umsatz_enc varchar(2000) NULL;
-alter table rechnungen_basis add column rechnung_rka varchar(2000) NULL;
-alter table rechnungen_basis add column rechnung_vorherige_signatur varchar(2000) NULL;
-alter table rechnungen_basis add column rechnung_beleg_kennzeichen varchar(2000) NULL;
-alter table rechnungen_basis add column rechnung_istTrainingsBeleg bit NOT NULL;
+    alter table artikel_basis add column artikel_rksv bit NOT NULL;
+end;;
+call upgrade_db();;
+DROP PROCEDURE IF EXISTS upgrade_db;;
 
-alter table artikel_basis add column artikel_rksv bit NOT NULL;
--- version.py
+++ version.py
@@ -1,4 +1,4 @@
-VERSION=1157
+VERSION=1158
 
 if __name__ == "__main__":
 	print VERSION
